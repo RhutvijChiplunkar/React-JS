be9d840366c2f3c0102edadb55a5a67b854449e9
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-04-14 18:17:23 +0530

    changes made for props

--- src/MultipleComponents2.js
+++ src/MultipleComponents2.js
@@ -1,10 +1,19 @@
 import { useState } from 'react';
+import PropsEg from './PropsExample';
 
 const ListsOp = () => {
-    const[list,setList]=useState([
+    const[list,setLists]=useState([
         {name : 'Rhutvij', surname: 'Chiplunkar', id:1},
         {name : 'ABC', surname: 'XYZ', id:2},
         {name : 'UVW', surname: 'PQR', id:3}
+    ]); 
+
+    const[players,setPlayer]=useState([
+        {name : 'Virat', team: 'RCB', place:8},
+        {name : 'Rohit', team: 'MI', place:2},
+        {name : 'Dhoni', team: 'CSK', place:1},
+        {name : 'Rahul', team: 'PBKS', place:6},
+        {name : 'Jadeja', team: 'CSK', place:1}
     ]);
 
     return (  
@@ -17,6 +26,10 @@ const ListsOp = () => {
                 </div>
             ))
             }
+
+        {/* Props in React.js */}
+        <PropsEg players={players}/>
+        <PropsEg players={players.filter(player=>player.team==="CSK")} title="CSK team members"/>
         </div>
     );
 }
