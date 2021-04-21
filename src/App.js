859ffdc1af80f9bd4d4dfada8648f90b89552280
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-04-21 11:49:32 +0530

    necessary changes done to move all components created to 'Component' folder

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 //import logo from './logo.svg';
 import './App.css';
-import Test from './MultipleComponents';
-import ListsOp from './MultipleComponents2';
-import Teams from './teams';
+import Test from './Components/MultipleComponents';
+import ListsOp from './Components/MultipleComponents2';
+import Teams from './Components/teams';
+import './index.css'
 
   function App() {
-    var x=100;
+    /* var x=100;
     var y="Rhutvij Chiplunkar";
-    var link="https://www.cricbuzz.com/";
+    var link="https://www.cricbuzz.com/"; */
     return (
       <div className="App">
       <div className="title">
-        <h1 style={{color:'red',
-          backgroundColor:'black',
-          borderRadius:'100px'}}>Welcome to React</h1>
+        <h1>Welcome to React</h1>
       </div>
 
-      {/* Component created in other JS file and imported */}
+      {/* Component created in other JS file and imported 
       <Test></Test>
-      <h2>{x}</h2>     {/* dynamic values for variables*/}
+      <h2>{x}</h2>     {/* dynamic values for variables
       <h2>Creator of this page is {y}</h2>
 
       <h3>Random number between 1-100 is::{Math.floor(Math.random()*100)}</h3>
 
       <a href={link}>Click Here to go to CRICKBUZZ!!</a>
       <br></br>
+      
 
-
-      <ListsOp />
+      <ListsOp />*/}
       <Teams />
       </div>
   );
