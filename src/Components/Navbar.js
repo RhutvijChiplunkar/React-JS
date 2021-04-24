f2d968b7430ea9e7ce8fd11e962a2d0442d0d4a2
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-04-24 15:20:18 +0530

    changes made to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar= () => {
     return (  
         <div className="nav-bar">
+            <a href="https://www.cricbuzz.com/">CRICKBUZZ</a>
             <Link to="/">HOME</Link>
             <Link to="/list">LISTS</Link>
             <Link to="teams">TEAMS</Link>
