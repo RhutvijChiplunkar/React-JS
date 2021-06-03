a16a283f35556649d35897cfd10757551f15ac90
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-06-03 22:23:23 +0530

    Route links added for add to team database

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import ListsOp from './Components/MultipleComponents2';
 import PropsEg from './Components/PropsExample';
 import Teams from './Components/Teams';
 import Navbar from './Components/Navbar';
-import TeamList from './Components/TeamList'
+import TeamList from './Components/TeamList';
+import Forms from './Components/forms';
 import './App.css';
 import './index.css'
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import ListDetails from './Components/ListDetails';
+import AddTeam from './Components/IPLforms';
 
   function App() {
     /* var x=100;
@@ -27,6 +29,10 @@ import ListDetails from './Components/ListDetails';
              <h1>Welcome to React JS</h1> 
           </div>
 
+          <div>
+            <h1>This is the home page :)</h1>
+          </div>
+          
           <div className="main-paths">
             <Switch>
                 <Route exact path="/teams">
@@ -43,6 +49,14 @@ import ListDetails from './Components/ListDetails';
 
                 <Route exact path="/test">
                     <Test/>
+                </Route>
+
+                {/* <Route exact path="/forms">
+                  <Forms/>  
+                </Route> */}
+
+                <Route exact path="/addTeam">
+                  <AddTeam/>  
                 </Route>
 
             </Switch>
