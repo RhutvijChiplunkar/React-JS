5d91203c925431c47cb2eee9d7f54c6038aae24b
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-06-04 14:10:39 +0530

    DELETE team functionality added

--- src/Components/ListDetails.js
+++ src/Components/ListDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from './useFetch';
 
 const ListDetails = () => {
     const {id}=useParams();
     const{data:tm, error, isPending }=useFetch('http://localhost:8000/IPLteams/'+ id );
+    const hist=useHistory();
 
+    const handleClick=()=>{
+        fetch('http://localhost:8000/IPLteams/'+ tm.id,{
+            method:'DELETE'
+        }).then(()=>{
+            console.log("Team successfully deleted");
+            hist.push('/');
+        })
+    }
     return ( 
         <div className="list-details">
             {isPending && <div>Loading....</div>}
@@ -15,6 +24,7 @@ const ListDetails = () => {
                     <h2>Captain: {tm.captain}</h2>
                     <h3>IPL 2021 position: {tm.rank}</h3>
                     <h3>Trophies Won: {tm.trophies}</h3>
+                    <button onClick={handleClick}>Delete Team</button>
                 </article>
             )}
         </div>
