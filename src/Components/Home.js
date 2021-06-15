75c20c640fb20db7c4dc7cb4a48cad3af5c0ac26
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-06-15 23:58:21 +0530

    HOME component made

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="title">
+        <h1>Welcome to React JS</h1>
+        <h2>Home page</h2> 
+     </div>
+     );
+}
+ 
+export default Home;
